baadf96475d7fd16bf1d9ad4dcdf9a87a51dcb56
TolyaTalamanov <talamtolik@gmail.com>
2018-03-28 07:01:04 +0300

    Add fucntion which find max sum in half array, and shift iterator to start this sum

--- test/test_find_max_subarray.cpp
+++ test/test_find_max_subarray.cpp
@@ -4,6 +4,28 @@
 #include <gtest/gtest.h>
 #include "find_max_subarray.hpp"
 
+using std::reverse_iterator;
+
+TEST(FindMaxSubarray, FindMaxSubarrayInHalfArrayAndShiftMidIt) {
+	std::vector<int> v{-1000000, 1000, 1, 2, -3, 4, 5, 10, -15, 0, 100};
+	auto begin = v.begin();
+	auto end   = v.end();
+	auto mid   = begin;
+	std::advance(mid, std::distance(begin, end) / 2);
+	auto m_l = mid;
+	auto m_r = mid;
+	auto r_it_mid = reverse_iterator(m_l);
+	auto r_it_begin = reverse_iterator(begin);
+
+	auto max_l = FindMaxSubarrayInHalfArrayAndShiftMidIt(r_it_mid, r_it_begin);
+	auto max_r = FindMaxSubarrayInHalfArrayAndShiftMidIt(m_r, end);
+
+	EXPECT_EQ(1000, max_l);
+	EXPECT_EQ(1000, *r_it_mid);
+	EXPECT_EQ(104, max_r);
+	EXPECT_EQ(100, *m_r);
+}
+
 TEST(FindMaxSubarray, FindInPositiveArray) {
 	std::vector<int> v{1, 2, 3, 4, 5, 10, 15};
 	auto t = FindMaxSubarray(v.begin(), v.end());
